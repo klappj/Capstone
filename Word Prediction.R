9ce607774058212527f24cbf66a0da2d96f4f120
klappj <klappj@gmail.com>
2019-11-30 14:14:22 -0700

    Initial thoughts on the word prediction algorithm

---
+++ Word Prediction.R
@@ -0,0 +1,36 @@
+# This script is going to try to predict the next word given the previous 3
+# with luck, it will also be able to do some prediction as you type
+# but that is black magic.
+
+# this is the placeholder to load files
+# onegram
+# twogram
+# trigram
+# fourgram
+
+# Start with 3 word string, try to find that word in fourgram
+# ... 
+
+# find that string in the 3 gram to find n for denominator of odds
+
+# find the last 2 words in 3 gram
+
+# find the middle word to find n for denominator of oddds
+
+# find last word in dictionary
+
+# shit ... the onegrams have been snipped, so I might have to build a dictionary
+
+# that wasn't the intent, but it makes things faster, 
+# and the ngrams are very, very slow to build from the short corpus
+
+# given the odds, this function should return next word guesses in order
+
+
+# the next function should, given the list of guesses, and the letters the 
+# input is givng as typing, do a predictive typing. 
+# this might require building my own dictionary again, before the text was 
+# cleaned ... that would give word frequencies. Other thought is to just
+# find a dictionary somewhere else.
+
+
