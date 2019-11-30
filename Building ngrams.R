7dbe7df06a76c750d32025d3e22f6852947e4601
klappj <klappj@gmail.com>
2019-11-30 13:38:40 -0700

    Completed, but want to run again with a larger corpus.

---
+++ Building ngrams.R
@@ -0,0 +1,34 @@
+# This does the data analysis stuff from week 2.
+library(tm)
+library(RWeka)
+
+
+
+smallCorpus <- readRDS("cleanedCorpus.RDS")
+smallCorpusDF <-data.frame(text=unlist(sapply(smallCorpus,`[`, "content")), 
+                           stringsAsFactors = FALSE)
+
+# that didn't work, trying
+smallCorpusDF <- data.frame(smallCorpus$content, stringsAsFactors = FALSE)
+
+## Building the tokenization function for the n-grams
+ngramTokenizer <- function(theCorpus, ngramCount) {
+  ngramFunction <- NGramTokenizer(theCorpus, 
+                                  Weka_control(min = ngramCount, max = ngramCount, 
+                                               delimiters = " \\r\\n\\t.,;:\"()?!"))
+  ngramFunction <- data.frame(table(ngramFunction))
+  ngramFunction <- ngramFunction[order(ngramFunction$Freq, 
+                                       decreasing = TRUE),][1:10,]
+  colnames(ngramFunction) <- c("String","Count")
+  ngramFunction
+}
+
+onegram <- ngramTokenizer(smallCorpusDF, 1)
+saveRDS(onegram, "onegram.RD")
+digram <- ngramTokenizer(smallCorpusDF, 2)
+saveRDS(digram, file = "digram.RDS")
+trigram <- ngramTokenizer(smallCorpusDF, 3)
+saveRDS(trigram, file = "trigram.RDS")
+fourgram <- ngramTokenizer(smallCorpusDF, 4)
+saveRDS(fourgram, file = "fourgram.RDS")
+
