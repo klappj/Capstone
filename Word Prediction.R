a0320787a22c7c33283680e5e4e93641e6421d37
klappj <klappj@gmail.com>
2019-12-01 07:04:23 -0700

    Now has all the stuff to build the table. Need the logic for what exists and doesn't. This will become a nested if hell.

--- Word Prediction.R
+++ Word Prediction.R
@@ -2,11 +2,61 @@
 # with luck, it will also be able to do some prediction as you type
 # but that is black magic.
 
+library(tm)
+library(stringr)
+
+
+
 # this is the placeholder to load files
 # onegram
 # twogram
 # trigram
 # fourgram
+
+# clean up the input
+cleanInput <- function(text){
+  text <- removePunctuation(text)
+  text <- removeNumbers(text)
+  text <- str_replace_all(text, "[^[:alnum:]]", " ")
+  text <- stripWhitespace(text)
+  text <- tolower(text)
+  text <- stemDocument(text)
+  return(text)
+}
+
+
+input <- cleanInput(input)
+
+inputLength <- sapply(strsplit(input, " "), length)
+lastword <- word(input, -1)
+#if inputLength > 1
+last2word <- paste(word(input, -2), lastword, sep = " ")
+
+#if inputLength > 2
+last3word <- paste(word(input, -3), last2word, sep = " ")
+
+onegramCount <- onegram[onegram$String == lastword,]$Count
+digramCount <- digram[digram$String == last2word,]$Count
+trigramCount <- trigram[trigram$String == last3word,]$Count
+
+
+temp3 <- fourgram[fourgram$Three == last3word,]
+temp3$p <- temp3$Count / trigramCount
+
+temp2 <- trigram[trigram$Two == last2word,]
+temp2$p <- temp2$Count/digramCount
+
+temp1 <- digram[digram$One == lastword,]
+temp1$p <- temp1$Count / onegramCount
+
+# first hundred words should be good enough
+if (dim(temp3)[1] > 100) { temp3 <- temp3[1:100,]}
+if (dim(temp2)[1] > 100) { temp2 <- temp2[1:100,]}
+if (dim(temp1)[1] > 100) { temp1 <- temp1[1:100,]}
+
+guesses <- rbind(temp1[,c("String","Count","p")],temp2[,c("String","Count","p")],temp3[,c("String","Count","p")])
+
+# well, there you have it, guesses. Now just to put these in order with a loop.
 
 # Start with 3 word string, try to find that word in fourgram
 # ... 
@@ -14,6 +64,7 @@
 # find that string in the 3 gram to find n for denominator of odds
 
 # find the last 2 words in 3 gram
+
 
 # find the middle word to find n for denominator of oddds
 
